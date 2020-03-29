28190013301d1be9fec56f64ba347600f3cb6df2
Arthur White <mister.arthur.white@gmail.com>
2020-03-29 15:27:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, typeOfGift) {
+  let gratitudes = []
+  for (let i = 0; i <names.length ; i++) {
+        //console.log(`Thank you, ${names[i]}, for the wonderful ${typeOfGift} gift!`);
+        gratitudes.push(`Thank you, ${names[i]}, for the wonderful ${typeOfGift} gift!`);
+
+  }
+ return gratitudes;
+}
+
+function countDown(count) {
+  for (let i = count; i >= 0; i--) {
+    console.log(`${i}`);
+  }
+}
+
